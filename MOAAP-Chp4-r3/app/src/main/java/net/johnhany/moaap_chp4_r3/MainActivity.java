06ce9b12be7dbf57c3fbfe93d6269dad8d70e6f6
John Hany <johnhany@163.com>
2017-08-04 11:46:39 +0800

    Added chp4

---
+++ MOAAP-Chp4-r3/app/src/main/java/net/johnhany/moaap_chp4_r3/MainActivity.java
@@ -0,0 +1,192 @@
+package net.johnhany.moaap_chp4_r3;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean              mIsFrontCamera = false;
+    private MenuItem             mItemSwitchCamera = null;
+    private Mat mRgba;
+    private CascadeClassifier haarCascade;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    try{
+                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+                        File cascadeDir = getDir("cascade",
+                                Context.MODE_PRIVATE);
+                        File mCascadeFile = new File(cascadeDir,"cascade.xml");
+                        FileOutputStream os = new
+                                FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while((bytesRead = is.read(buffer)) != -1)
+                        {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        haarCascade = new CascadeClassifier(
+                                mCascadeFile.getAbsolutePath());
+                        if (haarCascade.empty())
+                        {
+                            Log.i("Cascade Error",
+                                    "级联分类器加载失败");
+                            haarCascade = null;
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Log.i("Cascade Error: ","Cascase not found");
+                    }
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_surface_view);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i(TAG, "called onCreateOptionsMenu");
+        mItemSwitchCamera = menu.add("Toggle Front/Back camera");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toastMessage;
+
+        if (item == mItemSwitchCamera) {
+            mOpenCvCameraView.setVisibility(SurfaceView.GONE);
+            mIsFrontCamera = !mIsFrontCamera;
+
+            if (mIsFrontCamera) {
+                mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_surface_view);
+                mOpenCvCameraView.setCameraIndex(1);
+                toastMessage = "Front Camera";
+            } else {
+                mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_surface_view);
+                mOpenCvCameraView.setCameraIndex(-1);
+                toastMessage = "Back Camera";
+            }
+
+            mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+            mOpenCvCameraView.setCvCameraViewListener(this);
+            mOpenCvCameraView.enableView();
+            Toast toast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        //Rotating the input frame
+        Mat mGray = inputFrame.gray();
+        mRgba = inputFrame.rgba();
+        if (mIsFrontCamera)
+        {
+            Core.flip(mRgba, mRgba, 1);
+        }
+
+
+        //Detecting face in the frame
+        MatOfRect faces = new MatOfRect();
+        if(haarCascade != null)
+        {
+            haarCascade.detectMultiScale(mGray, faces, 1.1, 2, 2, new Size(200,200), new Size());
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(100), 3);
+        return mRgba;
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+}
